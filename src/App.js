7b2af62304e965504d33dcdd0701653a883e15b7
yuki3738 <y.minamiya@gmail.com>
2020-10-28 01:39:31 +0900

    フォームの操作とイベントハンドリング
    https://zenn.dev/likr/articles/6be53ca64f29aa035f07#%E3%83%95%E3%82%A9%E3%83%BC%E3%83%A0%E3%81%AE%E6%93%8D%E4%BD%9C%E3%81%A8%E3%82%A4%E3%83%99%E3%83%B3%E3%83%88%E3%83%8F%E3%83%B3%E3%83%89%E3%83%AA%E3%83%B3%E3%82%B0

--- src/App.js
+++ src/App.js
@@ -47,6 +47,35 @@ function Gallery(props) {
   );
 }
 
+function Form(props) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const { breed } = event.target.elements;
+    props.onFormSubmit(breed.value);
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="field has-addons">
+          <div className="control is-expanded">
+            <div className="select is-fullwidth">
+              <select name="breed" defaultValue="shiba">
+                <option value="shiba">Shiba</option>
+                <option value="akita">Akita</option>
+              </select>
+            </div>
+          </div>
+          <div className="control">
+            <button type="submit" className="button is-dark">
+              Reload
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+
+  )
+}
 function Main() {
   const [urls, setUrls] = useState(null);
   useEffect(() => {
@@ -54,8 +83,18 @@ function Main() {
       setUrls(urls);
     });
   }, []);
+  function reloadImages(breed) {
+    fetchImages(breed).then((urls) => {
+      setUrls(urls);
+    });
+  }
   return (
     <main>
+      <section className="section">
+        <div className="container">
+          <Form onFormSubmit={reloadImages} />
+        </div>
+      </section>
       <section className="section">
         <div className="container">
           <Gallery urls={urls} />
