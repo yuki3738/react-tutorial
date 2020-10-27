7eb54993d0d70ef640068bf95680133c719df3a9
yuki3738 <y.minamiya@gmail.com>
2020-10-27 23:22:29 +0900

    式の埋め込み
    https://zenn.dev/likr/articles/6be53ca64f29aa035f07#%E5%BC%8F%E3%81%AE%E5%9F%8B%E3%82%81%E8%BE%BC%E3%81%BF

--- src/App.js
+++ src/App.js
@@ -11,14 +11,12 @@ function Header() {
 }
 
 function Image() {
+  const url = "https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
   return (
     <div className="card">
       <div className="card-image">
         <figure className="image">
-          <img
-            src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
-            alt="cute dog!"
-          />
+          <img src={url} alt="cute dog!" />
         </figure>
       </div>
     </div>
