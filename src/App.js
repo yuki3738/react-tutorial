f24924c64de1fc196a0ef4c597ad4c5d7ed4e9c5
yuki3738 <y.minamiya@gmail.com>
2020-10-27 23:36:28 +0900

    繰り返し
    https://zenn.dev/likr/articles/6be53ca64f29aa035f07#%E7%B9%B0%E3%82%8A%E8%BF%94%E3%81%97

--- src/App.js
+++ src/App.js
@@ -22,23 +22,41 @@ function Image(props) {
   );
 }
 
-function Gallery() {
-  const url = "https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
+function Gallery(props) {
+  const { urls } = props;
   return (
     <div className="columns is-vcentered is-multiline">
-      <div className="column is-3">
-        <Image src={url} />
-      </div>
+    {urls.map((url) => {
+      return (
+        <div key={url} className="column is-3">
+          <Image src={url} />
+        </div>
+      );
+    })}
     </div>
   );
 }
 
 function Main() {
+  const urls = [
+    "https://images.dog.ceo/breeds/shiba/shiba-11.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-12.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-14.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-17.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-2.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-3i.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-4.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-5.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-6.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-7.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-8.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-9.jpg",
+  ];
   return (
     <main>
       <section className="section">
         <div className="container">
-          <Gallery />
+          <Gallery urls={urls} />
         </div>
       </section>
     </main>
